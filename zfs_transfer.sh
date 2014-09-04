70b04946c5b6f962463abf67566ea80acb859ce2
Jaakko Salo <jvsalo@cc.hut.fi>
2014-09-05 00:29:18 +0300

    Fixing comment placement...

--- zfs_transfer.sh
+++ zfs_transfer.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # The MIT License (MIT)
 
 # Copyright (c) 2014 Jaakko Salo
@@ -19,8 +21,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
-#!/bin/bash
 
 # Mandatory options:
 # --remote-host <host> - remote host
