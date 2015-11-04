0ccc17031e6b9f8e6b622156696e343080417bfb
Jaakko Salo <jaakko@tuxera.com>
2015-11-05 00:21:32 +0200

    Fixing indent.

--- zfs_transfer.sh
+++ zfs_transfer.sh
@@ -2,7 +2,7 @@
 
 # The MIT License (MIT)
 
-# Copyright (c) 2014 Jaakko Salo
+# Copyright (c) 2014-2015 Jaakko Salo
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -45,7 +45,11 @@ datasetof() {
 }
 
 # Script begins here
-if ! options=$(getopt -o h:r:l:k:b:p:t:n -l remote-host:,remote-fs:,local-fs:,keep-days:,mbuffer:,port:,tag:,no-pigz -- "$@") ; then
+
+sopts="h:r:l:k:b:p:t:n"
+lopts="remote-host:,remote-fs:,local-fs:,keep-days:,mbuffer:,port:,tag:,no-pigz"
+
+if ! options=$(getopt -o $sopts -l $lopts -- "$@") ; then
     echo "Invalid options."
     exit 1
 fi
@@ -90,7 +94,8 @@ if [ "$remote_host" = "localhost" ] ; then
 fi
 
 # Generate lockfile based on target dataset name
-lockf=/var/lock/zfs_replicate_$(echo "$remote_host" "$remote_fs" | md5sum | cut -d' ' -f 1) || exit 1
+lockf=/var/lock/zfs_replicate_$(echo "$remote_host" "$remote_fs" | md5sum | \
+    cut -d' ' -f 1) || exit 1
 
 (
     if ! flock -n -x 9; then
@@ -98,29 +103,41 @@ lockf=/var/lock/zfs_replicate_$(echo "$remote_host" "$remote_fs" | md5sum | cut 
     fi
 
     # Get remote snapshots, extract most recent snapshot
-    remote_list=$($ssh "zfs list -H -r -d 1 -t snapshot -o name -s creation \"$remote_fs\"") && [ -n "$remote_list" ] || \
-        { echo "Unable to get remote snapshots."; exit 1; }
+    remote_list=$($ssh "zfs list -H -r -d 1 -t snapshot -o name \
+        -s creation \"$remote_fs\"")
+
+    if [ $? -ne "0" ] || [ -z "$remote_list" ]; then
+        echo "Unable to get remote snapshots."
+        exit 1
+    fi
 
     remote_tip=$(echo "$remote_list" | tail -n 1)
 
     # Get local snapshots, if available
-    local_list=$(zfs list -H -r -d 1 -t snapshot -o name -s creation "$local_fs")
+    local_list=$(zfs list -H -r -d 1 -t snapshot -o name -s creation \
+        "$local_fs")
 
     # Receive full stream or incremental?
     if [ $? -ne 0 ] || [ -z "$local_list" ] ; then
         echo "Can't get local snapshots. Attempting initial send+receive..."
 
         # Create subdatasets if needed, then send+receive full stream
-        zfs create -p "$local_fs" || { echo "Can't create local filesystem."; exit 1; }
-        $ssh "zfs send -R \"$remote_tip\" | $pigz" | $mbuffer | $gunzip | zfs receive -F -u "$local_fs" || \
+        zfs create -p "$local_fs" || \
+            { echo "Can't create local filesystem."; exit 1; }
+
+        $ssh "zfs send -R \"$remote_tip\" | $pigz" | $mbuffer | $gunzip | \
+            zfs receive -F -u "$local_fs" || \
             { echo "send/receive failed. Unable to continue."; exit 1; }
 
-        # Put the remote tip on hold after successful receive. If this fails, you should retry after creating
-        # new snapshot(s) on the remote site. Alternatively, you should destroy the recently received snapshot
-        # on local site, that does not have a hold on the remote server. Otherwise, you risk snapshot tip expiring
-        # on remote site.
+        # Put the remote tip on hold after successful receive. If this fails,
+        # you should retry after creating new snapshot(s) on the remote site.
+        # Alternatively, you should destroy the recently received snapshot on
+        # local site, that does not have a hold on the remote server. Otherwise,
+        # you risk snapshot tip expiring on remote site.
+
         $ssh "zfs hold -r $tag \"$remote_tip\"" || \
-            { echo "*** WARNING! *** Couldn't hold remote snapshot. Please re-run to fix this."; exit 1; }
+            { echo "*** WARNING! *** Couldn't hold remote snapshot. Please" \
+                "re-run to fix this."; exit 1; }
 
         exit 0
     else
@@ -132,23 +149,30 @@ lockf=/var/lock/zfs_replicate_$(echo "$remote_host" "$remote_fs" | md5sum | cut 
         [ "$(snapof "$local_tip")" == "$(snapof "$remote_tip")" ] && exit 0
 
         # Send+receive incremental, put remote tip on hold.
-        if ! $ssh "zfs send -R -i \"$remote_source\" \"$remote_tip\" | $pigz" | $mbuffer | $gunzip | zfs receive -u "$local_fs" ; then
-		echo "send/receive failed. Destroying partially received data (in case children got updated) and exiting. Most errors below are OK.";
-		zfs list -r -H -o name "$local_fs" | while read dataset ; do
-			zfs rollback -r "${dataset}@$(snapof "$local_tip")"
-		done
-		exit 1
-	fi
+        if ! $ssh "zfs send -R -i \"$remote_source\" \"$remote_tip\" | $pigz" |\
+            $mbuffer | $gunzip | zfs receive -u "$local_fs" ; then
+            echo "send/receive failed. Destroying partially received data (in" \
+                 "case children got updated) and exiting. Most errors below" \
+                 "are OK.";
+
+            zfs list -r -H -o name "$local_fs" | while read dataset ; do
+                zfs rollback -r "${dataset}@$(snapof "$local_tip")"
+            done
+            exit 1
+        fi
 
         # Above comment about the warning applies here too.
         $ssh "zfs hold -r $tag \"$remote_tip\"" || \
-            { echo "*** WARNING! *** Couldn't hold remote snapshot. Please re-run to fix this."; exit 1; }
+            { echo "*** WARNING! *** Couldn't hold remote snapshot. Please" \
+                "re-run to fix this."; exit 1; }
 
-        # Drop the hold from previous snapshot. If connectivity fails here, it will still be destroyed before
-	# the local snapshot is destroyed (next loop).
+        # Drop the hold from previous snapshot. If connectivity fails here, it
+        # will still be destroyed before the local snapshot is destroyed
+        # (next loop).
         $ssh "zfs release -r $tag \"$remote_source\""
 
-        # Destroy expired snapshots from local site after ensuring there is no hold on the remote server.
+        # Destroy expired snapshots from local site after ensuring there is no
+        # hold on the remote server.
         now=$(date '+%s')
 
         echo "$local_list" | while read snap; do
@@ -159,17 +183,23 @@ lockf=/var/lock/zfs_replicate_$(echo "$remote_host" "$remote_fs" | md5sum | cut 
             [ $? -ne 0 ] || [ -z "$creation" ] && break
             [ "$(($now - $creation))" -gt $keep_secs ] || break
 
-            # If snap should be destroyed, make sure there is no hold on remote site.
-            # First test if the snapshot exists at all. Note that ssh might fail, but
-            # that must not be interpreted as "snapshot doesn't exist on remote site"!
+            # If snap should be destroyed, make sure there is no hold on
+            # remote site. First test if the snapshot exists at all. Note that
+            # ssh might fail, but that must not be interpreted as
+            # "snapshot doesn't exist on remote site"!
 
-            res=$($ssh "zfs list \"$remote_snap\" >/dev/null 2>&1 && echo Y || echo N") || break
+            res=$($ssh \
+                "zfs list \"$remote_snap\" >/dev/null 2>&1 && echo Y || echo N"\
+                ) || break
+
             if [ "$res" == "Y" ] ; then
                 $ssh "zfs release -r $tag \"$remote_snap\"" >/dev/null 2>&1
                 holds="$($ssh "zfs holds -H \"$remote_snap\"")" || continue
-                echo "$holds" | awk '{print $2}' | grep "^${tag}\$" > /dev/null && continue
+                echo "$holds" | awk '{print $2}' | grep "^${tag}\$" \
+                    > /dev/null && continue
             fi
 
+            # Now the snapshot is 1. expired and 2. not held on remote host
             zfs destroy -r "$snap"
         done
     fi
